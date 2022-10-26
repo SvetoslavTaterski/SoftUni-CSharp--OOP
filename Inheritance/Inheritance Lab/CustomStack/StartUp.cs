69804749eff1df3ff32915b21a482446570e16fb
SvetoslavTaterski <svetoslav0247@gmail.com>
2022-10-26 15:52:25 +0300

    Initial commit

---
+++ Inheritance/Inheritance Lab/CustomStack/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            stack.AddRange(new string[4] { "1", "2", "3", "4" });
+            stack.AddRange(new string[4] { "5", "6", "7", "8" });
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
