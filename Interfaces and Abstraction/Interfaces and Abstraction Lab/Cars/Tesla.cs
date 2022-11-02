3f1685ae70ed47c1a81a5c79e189ae8810a68244
SvetoslavTaterski <svetoslav0247@gmail.com>
2022-11-02 17:27:48 +0200

    Interfaces And Abstraction Lab

---
+++ Interfaces and Abstraction/Interfaces and Abstraction Lab/Cars/Tesla.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public int Battery
+        {
+            get
+            {
+                return battery;
+            }
+            set
+            {
+                battery = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public string Start() => "Engine start";
+
+        public string Stop() => "Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
