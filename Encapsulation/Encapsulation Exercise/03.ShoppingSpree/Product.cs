131aa2064bf4cd63c91086de7c593cf995f4438f
SvetoslavTaterski <svetoslav0247@gmail.com>
2022-11-02 17:37:16 +0200

    Fixed ShoppingSpree Task

--- Encapsulation/Encapsulation Exercise/03.ShoppingSpree/Product.cs
+++ Encapsulation/Encapsulation Exercise/03.ShoppingSpree/Product.cs
@@ -40,6 +40,10 @@ namespace _03.ShoppingSpree
             }
             private set
             {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
                 cost = value;
             }
         }
