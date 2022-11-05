0d70bf51e7e0bf7e470a6a906db10e569ffc9846
SvetoslavTaterski <svetoslav0247@gmail.com>
2022-11-05 17:20:15 +0200

    Polymorphism Lab

---
+++ Polymorphism/Polymorphism Lab/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        public Animal(string name, string favoriteFood)
+        {
+            this.name = name;
+            this.favoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {name} and my fovourite food is {favoriteFood}";
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism Lab/Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (width + height);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
